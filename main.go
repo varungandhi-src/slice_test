860678fc9ed83eb245e26cd102228045760bad21
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-08-11 11:31:16 +0800

    More checks for new packages

--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import "fmt"
 import "slices"
+import "maps"
+import "cmp"
+import "log/slog"
 
 func main() {
+  _ = maps.Clone(map[string]int{})
+  _ = cmp.Compare(1, 2)
+  slog.Info("hello", "count", 3)
   fmt.Printf("%v\n", slices.Max([]int{1, 2, 3}))
 }
